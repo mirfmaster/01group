b6fe038c7d3d67774e2fe45f45fa064db23e2959
Muhamad Iqbal <mirfmaster@gmail.com>
2023-02-03 15:08:30 +0700

    add POC for error pino

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify';
-import swaggerPlugin from '@fastify/swagger';
-import swaggerPluginUi from '@fastify/swagger-ui';
-import fastifyPrintRoutes from 'fastify-print-routes';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = fastify();
+const app = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        colorize: true,
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+});
 
 app.get('/', async (req, res) => {
   return { message: 'Hello API' };
@@ -13,43 +21,6 @@ app.get('/', async (req, res) => {
 
 const start = async () => {
   try {
-    await app.register(fastifyPrintRoutes);
-    await app.register(swaggerPlugin, {
-      swagger: {
-        info: {
-          title: 'Doc',
-          description: 'Doc',
-          version: '1.0.0',
-        },
-        host: 'localhost:3000',
-        schemes: ['http'],
-        consumes: ['application/json'],
-        produces: ['application/json'],
-        definitions: {},
-        securityDefinitions: {
-          bearerAuth: {
-            type: 'apiKey',
-            name: 'Authorization',
-            in: 'header',
-          },
-        },
-      },
-    });
-
-    await app.register(swaggerPluginUi, {
-      routePrefix: '/documentation',
-      uiConfig: {
-        docExpansion: 'full',
-        deepLinking: false,
-      },
-      staticCSP: true,
-      transformStaticCSP: (header) => header,
-      transformSpecification: (swaggerObject, _request, _reply) => {
-        return swaggerObject;
-      },
-      transformSpecificationClone: true,
-    });
-
     await app.listen({ port });
     console.log(`[ ready ] http://localhost:${port}`);
   } catch (err) {
